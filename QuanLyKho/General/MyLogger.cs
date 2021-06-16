88dba659a23b6b2afbaec05e456c02797a1500ac
phongvan381989 <33854242+phongvan381989@users.noreply.github.com>
2021-06-17 05:30:40 +0700

    Create project

---
+++ QuanLyKho/General/MyLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.General
+{
+    /// <summary>
+    /// Logging.
+    /// </summary>
+    public class MyLogger
+    {
+        private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Get logger.
+        /// </summary>
+        /// <returns>The instance.</returns>
+        public static log4net.ILog GetInstance()
+        {
+            return Logger;
+        }
+
+        /// <summary>
+        /// Write log.
+        /// </summary>
+        /// <param name="message">Save this to log file.</param>
+        public static void Write(string message)
+        {
+            Logger.Info(message);
+        }
+    }
+}

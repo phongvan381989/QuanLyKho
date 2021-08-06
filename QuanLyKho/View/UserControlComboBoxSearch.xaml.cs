264361bccaab19f18153947294ab8b98f61bccf0
phongvan381989 <33854242+phongvan381989@users.noreply.github.com>
2021-08-06 18:01:35 +0700

    from the Vista

--- QuanLyKho/View/UserControlComboBoxSearch.xaml.cs
+++ QuanLyKho/View/UserControlComboBoxSearch.xaml.cs
@@ -103,32 +103,34 @@ namespace QuanLyKho.View
             TextBox textBox = (TextBox)((ComboBox)sender).Template.FindName("PART_EditableTextBox", (ComboBox)sender);
             string str = textBox.Text;
             string str1 = textBox.SelectedText;
-            //if(iCarret >= 0)
-                textBox.SelectionStart = iCarret;
-            //else
-                //textBox.SelectionStart = textBox.CaretIndex;
+            textBox.SelectionStart = iCarret;
             textBox.SelectionLength = 0;
         }
 
         private void ComboBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //TextBox textBox = (TextBox)((ComboBox)sender).Template.FindName("PART_EditableTextBox", (ComboBox)sender);
-            //iCarret = textBox.CaretIndex;
-        }
-
-        /// <summary>
-        /// Lấy được carret trước khi combobox sổ list, dẫn tới select text ở ô input text là thay đổi carret
-        /// </summary>
-        public void GetCarretBeforeIsDropDownOpenTrue()
-        {
-            TextBox textBox = (TextBox)MyComboxSearch.Template.FindName("PART_EditableTextBox", (ComboBox)MyComboxSearch);
-            iCarret = textBox.CaretIndex;
-        }
-
-        private void MyComboxSearch_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
             TextBox textBox = (TextBox)((ComboBox)sender).Template.FindName("PART_EditableTextBox", (ComboBox)sender);
             iCarret = textBox.CaretIndex;
+            BindingExpression bd = ((ComboBox)sender).GetBindingExpression(ComboBox.TextProperty);
+            if(bd != null)
+            {
+                bd.UpdateSource();
+            }
         }
+
+        ///// <summary>
+        ///// Lấy được carret trước khi combobox sổ list, dẫn tới select text ở ô input text là thay đổi carret
+        ///// </summary>
+        //public void GetCarretBeforeIsDropDownOpenTrue()
+        //{
+        //    TextBox textBox = (TextBox)MyComboxSearch.Template.FindName("PART_EditableTextBox", (ComboBox)MyComboxSearch);
+        //    iCarret = textBox.CaretIndex;
+        //}
+
+        //private void MyComboxSearch_PreviewKeyUp(object sender, KeyEventArgs e)
+        //{
+        //    TextBox textBox = (TextBox)((ComboBox)sender).Template.FindName("PART_EditableTextBox", (ComboBox)sender);
+        //    iCarret = textBox.CaretIndex;
+        //}
     }
 }
